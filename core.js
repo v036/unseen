4b6b9b6dc2c38190161534e93ba2fed38fdad66f
36 <102698287+v036@users.noreply.github.com>
2022-11-20 18:17:22 +0100

    Add files via upload

--- core.js
+++ core.js
@@ -3,16 +3,18 @@ const http = new XMLHttpRequest();
 const title = document.getElementById("title");
 const favicon = document.getElementById("favicon")
 const wrapper = document.getElementById("wrapper");
+const url = document.getElementById("url");
 
 http.onload = function(context) {
-    const rawmetadata = context.target.responseText;
+    var rawmetadata = context.target.responseText;
     const metadata = JSON.parse(rawmetadata);
     
     title.innerText = metadata.home.title;
     favicon.setAttribute("href", metadata.home.favicon);
+    url.setAttribute("content", metadata.home.url);
 }
 
-http.open("GET", "./meta.json", true);
+http.open("GET", "/meta.json", true);
 http.send();
 
 setTimeout(() => {
