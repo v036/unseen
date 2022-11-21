a5b958b723542077171931a56902600e81286d7e
36 <102698287+v036@users.noreply.github.com>
2022-11-21 11:02:14 +0100

    Add files via upload

--- scripts/core.js
+++ scripts/core.js
@@ -9,9 +9,9 @@ http.onload = function(context) {
     var rawmetadata = context.target.responseText;
     const metadata = JSON.parse(rawmetadata);
     
-    title.innerText = metadata.home.title;
-    favicon.setAttribute("href", metadata.home.favicon);
-    discord.setAttribute("href", metadata.home.discord)
+    title.innerText = metadata.scripts.title;
+    favicon.setAttribute("href", metadata.scripts.favicon);
+    discord.setAttribute("href", metadata.scripts.discord)
 }
 
 http.open("GET", "/meta.json", true);
